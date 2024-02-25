51d776c68fea5bcf9827ff667c91efbded9938b3
Vlaimir Korba <krissky1986@gmail.com>
2024-02-25 22:52:45 +0200

    #95 - Using Fragment in Route | Angular Router & Route Guards |

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
 import { PopularComponent } from './popular/popular.component';
 import { TestimonyComponent } from './testimony/testimony.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -18,4 +19,19 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.activeRoute.fragment.subscribe((data) => {
+      // console.log(data);
+      this.JumpToSection(data);
+    });
+  }
+
+  JumpToSection(sectionValue) {
+    document
+      .getElementById(sectionValue)
+      .scrollIntoView({ behavior: 'smooth' });
+  }
+}
