e1499daa3c8abce6c2afdb078636c0d1148f4a03
Vlaimir Korba <krissky1986@gmail.com>
2024-02-25 13:35:10 +0200

    #87 - Implementing NotFound Route | Angular Router & Route Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CoursesComponent } from './courses/courses.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
   { path: 'About', component: AboutComponent },
   { path: 'Contact', component: ContactComponent },
   { path: 'Courses', component: CoursesComponent },
+  { path: '**', component: NotFoundComponent },
 ];
