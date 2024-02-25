9b5d18f5af2e1cce5fabb3c638446998bdcabb8a
Vlaimir Korba <krissky1986@gmail.com>
2024-02-25 15:32:30 +0200

    #91 - Navigating between Routes Programmatically | Router & Route Guards |

--- src/app/home/popular/popular.component.ts
+++ src/app/home/popular/popular.component.ts
@@ -2,7 +2,8 @@ import { Component, inject } from '@angular/core';
 import { CourseService } from '../../Services/course.service';
 import { CommonModule } from '@angular/common';
 import { Course } from '../../Models/course';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-popular',
   standalone: true,
@@ -13,10 +14,22 @@ import { RouterLink } from '@angular/router';
 export class PopularComponent {
   courseService = inject(CourseService);
   popularCourses: Course[] = [];
+  router: Router = inject(Router);
+  // create if you need a relative path
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
 
   ngOnInit() {
     this.popularCourses = this.courseService.courses.filter(
       (c) => c.rating >= 4.5
     );
+  }
+
+  navigateToCourses() {
+    // by default in these methods the path is absolute
+    // this.router.navigate(['Courses'])
+    this.router.navigateByUrl('Courses');
+
+    // create if you need a relative path
+    // this.router.navigate(['Courses'], { relativeTo: this.activeRoute });
   }
 }
