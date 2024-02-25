ad75442ceab518914268266214d380a248086629
Vlaimir Korba <krissky1986@gmail.com>
2024-02-25 15:02:06 +0200

    #90 - Relative vs Absolute Route Path | Angular Router & Route Guards

--- src/app/home/popular/popular.component.ts
+++ src/app/home/popular/popular.component.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CourseService } from '../../Services/course.service';
 import { CommonModule } from '@angular/common';
 import { Course } from '../../Models/course';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-popular',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './popular.component.html',
   styleUrl: './popular.component.scss',
 })
